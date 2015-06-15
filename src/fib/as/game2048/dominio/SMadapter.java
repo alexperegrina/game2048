5be0be9a23268c2c93c2eac926d07e96b0fa1ff1
Eric Monforte <choteomental@gmail.com>
2015-06-15 16:16:36 +0200

    msg

---
+++ src/fib/as/game2048/dominio/SMadapter.java
@@ -0,0 +1,9 @@
+package fib.as.game2048;
+
+public class SMadapter {
+	public void enviarMissatge(Integer id, Integer punt, String mail)
+	{
+		String missatge = "Partida amb id "+id+" ha obtingut "+punt+" punts";
+		//enviarMissatge(missatge,mail);
+	}
+}
